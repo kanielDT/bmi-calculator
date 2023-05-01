1a75c3a486a3f610b44c3dca741827b5d468e2ea
Kaniel D. Tapper <kanieldannn@gmail.com>
2023-04-30 22:03:49 -0400

    first commit: Created a working BMI Calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+
+import { useState, useMemo } from 'react';
+import React from 'react';
 import './App.css';
 
+import "bootstrap/dist/css/bootstrap.css"
+
 function App() {
+
+  const [height, setHeight] = useState(1)
+  const [weight, setWeight] = useState(1)
+
+  function onWeightChange(e) {
+    const inputWeight = e.target.value
+    setWeight(inputWeight)
+  }
+
+  function onHeightChange(e) {
+    const inputHeight = e.target.value
+    setHeight(inputHeight)
+  }
+
+  const output = useMemo(() => {
+    return ((weight / (height * height)) * 703).toFixed(2)
+  }, [weight, height])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <h1>BMI Calculator</h1>
+        <div className="imput-section">
+          <p class="slider-output">Weight: {weight}</p>
+          <input className="input-slider"
+            type="range"
+            onChange={onWeightChange}
+            step="1"
+            min="1"
+            max="400"
+          />
+          <p className="input-slider">Height: {height}</p>
+          <input
+            type="range"
+            onChange={onHeightChange}
+            min="1"
+            max="96" />
+        </div>
+        <div className="output-section">
+          <p className="output">your BMI is {output}</p>
+        </div>
+      </main>
+      <div><img src="https://www.wcrf-uk.org/wp-content/uploads/2021/06/WCRF-BMI-chart-WEB.png" /></div>
+    </>
   );
 }
 
